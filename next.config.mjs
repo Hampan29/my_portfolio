a8ee9a3d12d31a82413ae02e7098ce8f967176c7
Hampan Gowda K L <hampangowda2934@gmail.com>
2026-09-19 11:31:33 +0530

    Enable GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+const repoName = 'my_portfolio';
+
 const nextConfig = {
+  output: 'export',
   reactStrictMode: true,
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+  basePath: isProd ? `/${repoName}` : '',
+  assetPrefix: isProd ? `/${repoName}/` : '',
   experimental: {
     typedRoutes: false,
   },
